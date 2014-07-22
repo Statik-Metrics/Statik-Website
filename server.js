3d9301cc87756264b6da2b7be49df3d1f93ab4f7
Gael Ribes <ribesg@yahoo.fr>
2014-07-22 11:59:40 +0200

    Add logging to server.js

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
-var app = require("./app.js"),
-    port = process.env.PORT || 8080;
+var express = require("express");
+var app = require("./app.js");
+
+var port = process.env.PORT || 8080;
+var log = process.env.LOG || false;
+
+if (log) {
+    var logFile = fs.createWriteStream(log, {flags: 'w'});
+    app.use(express.logger({stream: logFile}));
+    console.log("Using " + log + " for logging");
+} else {
+    console.log("Using stdout for logging");
+}
 
 app.listen(port);
 console.log("Statik Website started on port " + port);
